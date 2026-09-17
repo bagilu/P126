8fb3ee5de5dc103f778b80941b51d1901b3c57cc
C. M. Lin <bagilu@gmail.com>
2026-09-17 21:14:33 +0800

    Delete Website directory

--- Website/config.js
+++
@@ -1,7 +0,0 @@
-// 複製為 config.js；只填公開 anon key／publishable key。絕不可填 service_role。
-window.P126_CONFIG = {
- supabaseUrl: 'https://mfljkyvdadxlrbxlboce.supabase.co',
- supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs',
- // 改成實際 GitHub Pages 網址，並加入 Supabase Auth 的 Redirect URLs。
- redirectUrl: 'https://YOUR_ACCOUNT.github.io/P126/'
-};
